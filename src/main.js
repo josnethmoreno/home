e0c7ad6bd322984efce853fdb55048fa97ca9e30
josnethmoreno <josnethmoreno@gmail.com>
2022-09-22 22:44:52 -0400

    toggle

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 import './style.css'
+
+const toggleWork = document.getElementById('toggleWork')
+
+const changeValue = (e) => {
+	const value = toggleWork.getAttribute('data-value')
+	console.log(value)
+}
+toggleWork.addEventListener('click', (e) => changeValue(e))
